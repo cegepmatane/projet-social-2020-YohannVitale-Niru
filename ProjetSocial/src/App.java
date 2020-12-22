6c0ae9f9154b7811f7c41540c8db28114ecfbc5f
YohannVitale <31995754+YohannVitale@users.noreply.github.com>
2020-12-22 10:57:23 -0500

    Commentaire de mes teste DAO

--- ProjetSocial/src/App.java
+++ ProjetSocial/src/App.java
@@ -7,13 +7,13 @@ public class App {
 	public static void main(String[] parametres) 
 	{
 		MessageDAO dao = new MessageDAO();
-		dao.listerMessage();
+		/*dao.listerMessage();
 		Message message = new Message();
 		message.setAqui(2);
 		message.setDequi(1);
 		message.setMessageEnvoyer("Allo, comment ca va ? ");
-		dao.envoyerMessage(message);
-		//Fenetre.launch(Fenetre.class, parametres);
+		dao.envoyerMessage(message);*/
+		Fenetre.launch(Fenetre.class, parametres);
 	}
 
 }
